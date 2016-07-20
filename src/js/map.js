3771df20335814ae01dfc04e64a9f2c7fe813d99
Владислав Сазонов <vladsaz@i.ua>
2016-07-20 17:07:32 +0200

    Removed, because functionality was replaced into another file.

--- src/js/map.js
+++
@@ -1,8 +0,0 @@
-/*var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 48.429135, lng: 35.038124},
-    zoom: 18
-  });
-}*/
-//initMap();
