a28b73a132139799d4291e29bcc71d04b6748093
Владислав Сазонов <vladsaz@i.ua>
2016-07-26 18:31:27 +0300

    cleaned repository and fixed all minor bugs

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,14 @@ var prefix = require('gulp-autoprefixer');
 var uglify = require('gulp-uglifyjs');
 var filter = require('gulp-filter');
 var mainBowerFiles = require('gulp-main-bower-files');
+var ignore = require('gulp-ignore');
+var gulpUtil = require('gulp-util');
 
 gulp.task('default', ['css', 'js'], function () {
     console.log('Build successful');
+});
+gulp.task('libs', ['backbone', 'underscore', 'bootstrap', 'bootstrapjs', 'fontawesome','fontawesomefonts', 'glyphicons', 'jquery'], function () {
+    console.log('Libs copied');
 });
 
 gulp.task('css', function() {
@@ -31,6 +36,46 @@ gulp.task('js', function () {
     .pipe(gulp.dest('dist/js'));
 });
 
+gulp.task('glyphicons', function () {
+    return gulp.src('bower_components/glyphicons/fonts/*')
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('fontawesome', function () {
+    return gulp.src('bower_components/font-awesome/css/*.css')
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('fontawesomefonts', function () {
+    return gulp.src('bower_components/font-awesome/fonts/*')
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('jquery', function () {
+    return gulp.src('bower_components/jquery/dist/*.js')
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('bootstrap', function () {
+    return gulp.src('bower_components/bootstrap/dist/css/*.css')
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('bootstrapjs', function () {
+    return gulp.src('bower_components/bootstrap/dist/js/*.js')
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('backbone', function () {
+    return gulp.src('node_modules/backbone/*.js')
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('underscore', function () {
+    return gulp.src('node_modules/underscore/*.js')
+    .pipe(gulp.dest('dist/js'));
+});
+
 gulp.task('bower', function() {
     var filterJS = filter(['**/*.js'], { restore: true });
     var filterCSS = filter(['**/*.css'], { restore: true });
@@ -48,7 +93,9 @@ gulp.task('bower', function() {
         }))
         .pipe(filterJS)
         .pipe(concat('libs.min.js'))
-        .pipe(uglify())
+        //.pipe(ignore.exclude([ "*.map" ]))
+        //.pipe(uglify().on('error', gulpUtil.log))
+        //.pipe(uglify())
         .pipe(filterJS.restore)
         .pipe(filterCSS)
         .pipe(csso())
